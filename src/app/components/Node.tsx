8369d71a25d4b593226fb77b0a2f8037f2121e1f
HomHom1 <homahomen2@gmail.com>
2024-02-15 15:45:22 -0600

    changed coloring functionality

--- src/app/components/Node.tsx
+++ src/app/components/Node.tsx
@@ -1,17 +1,18 @@
-import {useState} from "react";
-
-
-export default function Node(props: {color: string, DataValue: any}) {
-
-
-
+export default function Node(props: { color: string, DataValue: any }) {
+    const bgColorClass = () => {
+        switch (props.color) {
+            case 'red': return 'bg-red-300';
+            case 'green': return 'bg-green-300';
+            default: return 'bg-green-50'; // Default color
+        }
+    };
 
     return (
         <div
             id={props.DataValue}
-            className={`bg-${props.color}-300 w-20 h-20 absolute flex items-center justify-center overflow-hidden bg-blue-50 rounded-full border-2 border-black hover:border-4 hover:border-gray-500`}
-
-        >{props.DataValue}</div>
-    )
+            className={`${bgColorClass()} w-20 h-20 absolute flex items-center justify-center overflow-hidden rounded-full border-2 border-black hover:border-4 hover:border-gray-500`}
+        >
+            {props.DataValue}
+        </div>
+    );
 }
-
